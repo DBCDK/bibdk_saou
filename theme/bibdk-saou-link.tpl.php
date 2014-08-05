45ab19fb3648284f56ba2391ca3bb0a527c4bfc2
pjohans <pjohans@gmail.com>
2014-08-05 14:20:58 +0200

    template for link

---
+++ theme/bibdk-saou-link.tpl.php
@@ -0,0 +1,8 @@
+<div class="dropdown-wrapper">
+<a class="dropdown-toggle" href="#">HEST</a>
+<?php if(!empty($link['message'])) :?>
+<div class="visuallyhidden dropdown-menu btn">
+    <?php echo $link['message'];?>
+</div>
+<?php endif;?>
+</div>
