92e9447dcb5ff92971539a5d4776fee369ba2aed
pjohans <pjohans@gmail.com>
2014-08-12 15:55:09 +0200

    javscript to handle ajax

---
+++ js/bibdk_saou.js
@@ -0,0 +1,40 @@
+(function ($) {
+
+  Drupal.bibdkHandleSaou = function(saou){
+    var link = $('#bibdk_saou_'+saou.selector);
+    if(saou.error.length > 0){
+      link.addClass('dropdown-toggle');
+      link.after(saou.error);
+    }
+    else{
+      link.replaceWith(saou.redirect);
+    }
+  }
+
+  Drupal.bibdkGetSaouRessource = function(ressource){
+    var link = ressource;
+    var pid = link.attr('data-pid');
+    var url = link.attr('data-url');
+    var destination = link.attr('href');
+
+    var request = $.ajax({
+      url:destination,
+      type: 'POST',
+      data: {
+        pid: pid
+      },
+      dataType: 'json',
+      success: Drupal.bibdkHandleSaou
+    });
+  }
+
+  Drupal.behaviors.bibdk_saou_load = {
+    attach: function (context) {
+      $('.soau-ressource-link', context).click(function (e) {
+        e.preventDefault();
+        Drupal.bibdkGetSaouRessource($(this));
+      });
+    }
+  };
+}
+      (jQuery));
