07abdd369c2895fc4375b03256eca2480e951b63
pjohans <pjohans@gmail.com>
2014-08-12 15:51:19 +0200

    refactore from template to render arrays

--- theme/bibdk-saou-link.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php dpm($variables);?>
-<div class="dropdown-wrapper">
-<a class="dropdown-toggle" href="#"><?php print $link['text']; ?></a>
-<?php if(!empty($link['message'])) :?>
-<div class="visuallyhidden dropdown-menu btn">
-    <?php echo $link['message'];?>
-</div>
-<?php endif;?>
-</div>
