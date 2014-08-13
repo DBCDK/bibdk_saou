18a399b75e4b903d06a1fde88c1d242126a56755
pjohans <pjohans@gmail.com>
2014-08-13 10:58:03 +0200

    show and hide throbber

--- js/bibdk_saou.js
+++ js/bibdk_saou.js
@@ -3,6 +3,8 @@
   Drupal.bibdkHandleSaou = function(saou){
     var link = $('#bibdk_saou_'+saou.selector);
     if(saou.error.length > 0){
+      // remove throbber
+      link.find('span').remove();
       link.addClass('dropdown-toggle');
       link.after(saou.error);
     }
@@ -13,6 +15,7 @@
 
   Drupal.bibdkGetSaouRessource = function(ressource){
     var link = ressource;
+    link.append('<span class="ajax-progress"><span class="throbber"></span></span>');
     var pid = link.attr('data-pid');
     var url = link.attr('data-url');
     var destination = link.attr('href');
